858ab716e9d383934d04ff0035892578c54d891c
Viet281101 <nva281101@gmail.com>
2024-05-17 06:46:58 +0200

    create polyomino.js

--- 2D/js/main.js
+++ 2D/js/main.js
@@ -90,30 +90,4 @@ class MainApp {
 	};
 };
 
-class Polyomino {
-	constructor(width, height, x, y, color) {
-		this.width = width;
-		this.height = height;
-		this.x = x;
-		this.y = y;
-		this.color = color;
-	};
-
-	draw(ctx, gridSize) {
-		ctx.fillStyle = this.color;
-		for (let i = 0; i < this.width; i++) {
-			for (let j = 0; j < this.height; j++) {
-				ctx.fillRect(this.x + i * gridSize, this.y + j * gridSize, gridSize, gridSize);
-				ctx.strokeRect(this.x + i * gridSize, this.y + j * gridSize, gridSize, gridSize);
-			}
-		}
-	};
-
-	contains(mouseX, mouseY, gridSize) {
-		return mouseX >= this.x && mouseX < this.x + this.width * gridSize &&
-			   mouseY >= this.y && mouseY < this.y + this.height * gridSize;
-	};
-};
-
 const main_app = new MainApp();
-
