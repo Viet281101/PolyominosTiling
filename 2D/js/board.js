216554acd3bb1e4c0c874c3fca10ed7851b89471
Viet281101 <nva281101@gmail.com>
2024-05-28 18:42:41 +0200

    start to collide blocks with grid #4

--- 2D/js/board.js
+++ 2D/js/board.js
@@ -5,6 +5,7 @@ export class GridBoard {
 		this.gridSize = gridSize;
 		this.rows = rows;
 		this.cols = cols;
+		this.grid = Array.from({ length: rows }, () => Array(cols).fill(null));
 		this.resizeCanvas();
 		this.drawGrid();
 
@@ -48,5 +49,62 @@ export class GridBoard {
 
 	clear() {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	};
+
+	isInBounds(polyomino) {
+		const { x, y, shape } = polyomino;
+		const gridSize = this.gridSize;
+		const offsetX = this.gridOffsetX;
+		const offsetY = this.gridOffsetY;
+
+		for (let i = 0; i < shape.length; i++) {
+			for (let j = 0; j < shape[i].length; j++) {
+				if (shape[i][j] === 1) {
+					const posX = Math.floor((x - offsetX + j * gridSize) / gridSize);
+					const posY = Math.floor((y - offsetY + i * gridSize) / gridSize);
+					if (posX < 0 || posX >= this.cols || posY < 0 || posY >= this.rows) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	};
+
+	isOverlapping(polyomino) {
+		const { x, y, shape } = polyomino;
+		const gridSize = this.gridSize;
+		const offsetX = this.gridOffsetX;
+		const offsetY = this.gridOffsetY;
+
+		for (let i = 0; i < shape.length; i++) {
+			for (let j = 0; j < shape[i].length; j++) {
+				if (shape[i][j] === 1) {
+					const posX = Math.floor((x - offsetX + j * gridSize) / gridSize);
+					const posY = Math.floor((y - offsetY + i * gridSize) / gridSize);
+					if (this.grid[posY][posX] !== null) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	};
+
+	placePolyomino(polyomino) {
+		const { x, y, shape, color } = polyomino;
+		const gridSize = this.gridSize;
+		const offsetX = this.gridOffsetX;
+		const offsetY = this.gridOffsetY;
+
+		for (let i = 0; i < shape.length; i++) {
+			for (let j = 0; j < shape[i].length; j++) {
+				if (shape[i][j] === 1) {
+					const posX = Math.floor((x - offsetX + j * gridSize) / gridSize);
+					const posY = Math.floor((y - offsetY + i * gridSize) / gridSize);
+					this.grid[posY][posX] = color;
+				}
+			}
+		}
 	};
 };
